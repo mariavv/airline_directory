b9533e85a46710ac3522ce9a5e187d040fe33242
mariavv <vasilevamariav@yandex.ru>
2021-08-27 14:43:19 +0400

    create project

---
+++ src/main/java/mariavv/airlinedirectory/AirlineDirectoryApplication.java
@@ -0,0 +1,13 @@
+package mariavv.airlinedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineDirectoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mariavv/airlinedirectory/AirlineDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package mariavv.airlinedirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineDirectoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
